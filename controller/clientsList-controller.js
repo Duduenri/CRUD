53a690df5ee59633aafbe198c1b16bf9ed4b7ea4
Duduenri <eduess290799@gmail.com>
2023-02-15 18:25:37 -0300

    final project

--- controller/clientsList-controller.js
+++ controller/clientsList-controller.js
@@ -8,7 +8,7 @@ const createNewLine = (nome, email) => {
         <td>${email}</td>
         <td>
             <ul class="tabela__botoes-controle">
-                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><a href="../screens/edit_customer.html" class="botao-simples botao-simples--editar">Editar</a></li>
                 <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
             </ul>
         </td> 
